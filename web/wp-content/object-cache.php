9acfaa1bd91f66c1c31d059bdb82012baecc5768
K. Adam White <kadamwhite@users.noreply.github.com>
2019-09-22 23:22:42 -0400

    Fix PHPCS

--- web/wp-content/object-cache.php
+++ web/wp-content/object-cache.php
@@ -1,3 +1,7 @@
 <?php
-# This is a Windows-friendly symlink
+/**
+ * Object Caching drop-in (see https://pantheon.io/docs/redis).
+ */
+
+// This is a Windows-friendly symlink
 require_once WP_CONTENT_DIR . '/plugins/wp-redis/object-cache.php';
