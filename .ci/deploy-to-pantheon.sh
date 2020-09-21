88e06313917997ba36cbf4d5e6f9ccd1e181be82
K Adam White <kadamwhite@users.noreply.github.com>
2020-09-21 00:26:54 -0400

    Attempt to update pantheon CI

--- .ci/deploy-to-pantheon.sh
+++ .ci/deploy-to-pantheon.sh
@@ -1,46 +1,37 @@
 #!/bin/bash
 
-if [[ ${CURRENT_BRANCH} != "master" && -z ${CI_PR_URL} ]];
+set -eo pipefail
+
+#
+# This script deploys the build artifact to Pantheon.
+# On the master branch the dev environment is used.
+# Otherwise a multidev environment is used.
+#
+
+# Authenticate with Terminus
+terminus -n auth:login --machine-token="$TERMINUS_TOKEN"
+
+# Prepare for Pantheon
+composer run prepare-for-pantheon
+
+if [[ $CI_BRANCH != $DEFAULT_BRANCH ]]
 then
-  echo -e "CI will only deploy to Pantheon if on the master branch or creating a pull requests.\n"
-  exit 0;
-fi
-
-set -ex
-
-TERMINUS_DOES_MULTIDEV_EXIST()
-{
-    # Return 1 if on master since dev always exists
-    if [[ ${CURRENT_BRANCH} == "master" ]]
-    then
-        return 1;
-    fi
-
-    # Stash list of Pantheon multidev environments
-    PANTHEON_MULTIDEV_LIST="$(terminus multidev:list -n ${TERMINUS_SITE} --format=list --field=Name)"
-
-    while read -r multiDev; do
-        if [[ "${multiDev}" == "$1" ]]
-        then
-            return 0;
-        fi
-    done <<< "$PANTHEON_MULTIDEV_LIST"
-
-    return 1;
-}
-
-if ! TERMINUS_DOES_MULTIDEV_EXIST ${TERMINUS_ENV}
-then
-    terminus env:wake -n "$TERMINUS_SITE.dev"
-    terminus build:env:create -n "$TERMINUS_SITE.dev" "$TERMINUS_ENV" --clone-content --yes --notify="$NOTIFY"
+    # Create a new multidev environment (or push to an existing one)
+    terminus -n build:env:create "$TERMINUS_SITE.dev" "$TERMINUS_ENV" --yes
 else
-    terminus build:env:push -n "$TERMINUS_SITE.$TERMINUS_ENV" --yes
+    # Push to the dev environment
+    terminus -n build:env:push "$TERMINUS_SITE.dev" --yes
 fi
 
-set +ex
-echo 'terminus secrets:set'
-terminus secrets:set -n "$TERMINUS_SITE.$TERMINUS_ENV" token "$GITHUB_TOKEN" --file='github-secrets.json' --clear --skip-if-empty
-set -ex
+# Run update-db to ensure that the cloned database is updated for the new code.
+terminus -n wp $TERMINUS_SITE.$TERMINUS_ENV -- core update-db
 
-# Cleanup old multidevs
-terminus build:env:delete:pr -n "$TERMINUS_SITE" --yes
+# Clear the site environment's cache
+terminus -n env:clear-cache "$TERMINUS_SITE.$TERMINUS_ENV"
+
+# Ensure secrets are set
+terminus -n secrets:set "$TERMINUS_SITE.$TERMINUS_ENV" token "$GITHUB_TOKEN" --file='github-secrets.json' --clear --skip-if-empty
+
+# Delete old multidev environments associated
+# with a PR that has been merged or closed.
+terminus -n build:env:delete:pr $TERMINUS_SITE --yes
